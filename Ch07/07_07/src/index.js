3f2e32bcbc4059ce25c6166d3173c4231edbde64
Test User <danielway91@live.com>
2018-05-18 23:35:43 -0700

    The initial commit for my React website built through lessons on Lynda, node-modules included for continuity of repo. Shrinkwrap included to lock modules to respective versions. Happy fun time.

---
+++ Ch07/07_07/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+ReactDOM.hydrate(
+  <App initialData={window.initialData} />,
+  document.getElementById('root')
+);
